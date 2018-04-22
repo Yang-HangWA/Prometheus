811d6b32d5c8d580df3b733475e15beef03bab28
Yang-HangWA <565901262@qq.com>
2018-04-22 22:38:12 +0800

    add quick_start and quick_stop file

---
+++ quick_start.sh
@@ -0,0 +1,13 @@
+sudo docker-compose up -d
+sudo nohup ./redis_exporter/redis_exporter  -redis.password penetration -redis.addr  redis://localhost:6379  > /dev/null &
+sudo docker run --name pg_exporter --net=host -e DATA_SOURCE_NAME="postgres://postgres:postgres@localhost:5432/?sslmode=disable" wrouesnel/postgres_exporter  > /dev/null &
+
+ti1=`date +%s`
+ti2=`date +%s`
+i=$(($ti2 - $ti1 ))
+while [[ "$i" -ne 3 ]]
+do 
+    ti2=`date +%s`
+    i=$(($ti2 - $ti1 ))
+done
+clear
---
+++ quick_stop.sh
@@ -0,0 +1,3 @@
+sudo docker-compose down 
+sudo pkill redis_exporter
+sudo docker rm -f pg_exporter
